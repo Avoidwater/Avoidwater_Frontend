395a7406291a1881ea90baecdbecceef65e7589c
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2024-07-17 17:20:36 +0900

    ⚡️::Nav 구현

--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import * as M from '../../style/Header/HeaderStyle';
+import { useNavigate } from "react-router-dom";
+
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const gotoMain = () => {
+    navigate("/main");
+  };
+
   return (
     <M.HeaderContainer>
-      <M.Logo />
+      <M.Logo onClick={gotoMain}/>
       <M.NearShelter to="/nearshelter">주변 대피소</M.NearShelter>
       <M.FloodingCar to="/floodingcar">침수 차량</M.FloodingCar>
       <M.DmgPreGuide to="/dmgpreguide">피해 예방 가이드</M.DmgPreGuide>
