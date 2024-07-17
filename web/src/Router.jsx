31da6a296c6489fdf7a0c345023592a33702fff9
0ccssu <kimsuaa0114@dgsw.hs.kr>
2024-07-17 10:47:28 +0900

    🎨:: 코드 구조 개선
    코드 구조 추가

---
+++ web/src/Router.jsx
@@ -0,0 +1,13 @@
+import { Router, Routes, Route } from "react-router-dom";
+
+const AppRouter = () => {
+    return(
+        <Router>
+            <Routes>
+                
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRouter;
