ba6a45188dd791f4b01e86796d25fbb51db33cbc
eungwan <166986618+silverconffin@users.noreply.github.com>
2024-07-17 23:02:24 +0900

    ⚡️:: 침수차량
    first commmit

--- web/src/style/SubmergedCar/SubmergedCar.js
+++ web/src/style/SubmergedCar/SubmergedCar.js
@@ -6,6 +6,10 @@ export const TitleTxt = styled.div`
   padding: 90px;
   margin-top: 20px;
 `;
+export const Jre1Container = styled.div`
+  padding: 50px;
+  margin-top: -120px;
+`;
 
 export const Jre1Title = styled.div`
   font-size: 24px;
@@ -21,6 +25,11 @@ export const Jre1Text = styled.div`
   margin-top: -150px;
 `;
 
+export const Jre2Container = styled.div`
+  padding: 50px;
+  margin-top: -180px;
+`;
+
 export const Jre2Title = styled.div`
   font-size: 24px;
   color: #4567e3;
@@ -33,4 +42,55 @@ export const Jre2Text = styled.div`
   color: #000000;
   padding: 90px;
   margin-top: -150px;
+`;
+
+export const Jre3Container = styled.div`
+  padding: 50px;
+  margin-top: -180px;
+`;
+
+export const Jre3Text = styled.div`
+  font-size: 24px;
+  color: #000000;
+  padding: 90px;
+  margin-top: -150px;
+`;
+
+export const Jre3Title = styled.div`
+  font-size: 24px;
+  color: #4567e3;
+  padding: 90px;
+  font-weight: bold;
+`;
+
+export const Jre4Container = styled.div`
+  padding: 50px;
+  padding-bottom: 20px;
+  margin-top: -180px;
+`;
+
+export const Jre4Text = styled.div`
+  font-size: 24px;
+  color: #000000;
+  padding: 90px;
+  margin-top: -150px;
+`;
+
+export const Jre4Title = styled.div`
+  font-size: 24px;
+  color: #4567e3;
+  padding: 90px;
+  font-weight: bold;
+`;
+
+export const JreListContainer = styled.div`
+  padding: 50px;
+  margin-top: -180px;
+`;
+export const JreList = styled.div`
+  padding: 120px;
+  margin-top: -180px;
+  color: #000000;
+  font-size: 24px;
+  line-height: 47.5px;
 `;
