5c0aeb9da54a76a94710bfcd55ae1d8ba93d8f08
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2024-07-17 20:46:54 +0900

    ✨:: kakaomap 호출

--- web/src/pages/Main/Main.jsx
+++ web/src/pages/Main/Main.jsx
@@ -1,32 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import map from "../../assets/img/map.png";
 import banner from "../../assets/img/banner.png";
 import car from "../../assets/img/car.png";
 import arrow from "../../assets/img/arrow.png";
-import locIcon from "../../assets/img/locIcon.png"
-
+import locIcon from "../../assets/img/locIcon.png";
+import { Map } from "react-kakao-maps-sdk";
 import * as M from "../../style/Main/Main";
 import Header from "../../components/Header/Header";
 
 function Main() {
+  const [state, setState] = useState({
+    center: { lat: 35.6632508239323, lng: 128.413618885714 },
+  });
+  const [searchAddress, setSearchAddress] = useState("");
+
+  useEffect(() => {
+    if (window.kakao && window.kakao.maps) {
+      SearchMap();
+    }
+  }, [searchAddress]);
+
+  const SearchMap = () => {
+    if (!window.kakao || !window.kakao.maps) {
+      console.error("Kakao Maps API is not loaded");
+      return;
+    }
+    const geocoder = new window.kakao.maps.services.Geocoder();
+    let callback = function (result, status) {
+      if (status === window.kakao.maps.services.Status.OK) {
+        const newSearch = result[0];
+        setState({
+          center: { lat: newSearch.y, lng: newSearch.x },
+        });
+      }
+    };
+    geocoder.addressSearch(`${searchAddress}`, callback);
+  };
+
   return (
     <>
       <Header />
       <M.GlobalStyle />
-      <M.map src={map} />
+      <Map
+        center={state.center}
+        style={{
+          width: "625px",
+          height: "500px",
+          position: "absolute",
+          border: "1px solid #D2D2D2",
+          marginLeft: "185px",
+          marginTop: "72px"
+
+        }}></Map>
+
       <M.banner src={banner} />
       <M.nearShelter>
         <M.shelterGuide>주변 대피소 위치 안내</M.shelterGuide>
         <M.line />
         <M.shelterDiv>
           <M.data1>울산교육연수연 지진해일 긴급대피장소</M.data1>
-          <M.locIcon1 src={locIcon}/>
+          <M.locIcon1 src={locIcon} />
           <M.semiData1>울산 북구 정자동</M.semiData1>
-          <M.locIcon2 src={locIcon}/>
+          <M.locIcon2 src={locIcon} />
           <M.data2>망양정옛터 지진해일 긴급대피장소</M.data2>
           <M.semiData2>울진 기성면</M.semiData2>
           <M.data3>화진휴게소 입구 지진해일 긴급대피장소</M.data3>
-          <M.locIcon3 src={locIcon}/>
+          <M.locIcon3 src={locIcon} />
           <M.semiData3>포항 북구 송라면</M.semiData3>
         </M.shelterDiv>
       </M.nearShelter>
@@ -39,4 +78,5 @@ function Main() {
     </>
   );
 }
+
 export default Main;
